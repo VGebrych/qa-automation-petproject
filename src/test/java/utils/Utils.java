9e547833bacdd34b83a8ce50f045543763624fda
vgebrych <v.gebrych@gmail.com>
2025-08-01 13:15:27 +0200

    Load global properties from GlobalData.properties and add utility method to retrieve values

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,6 +1,24 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Utils {
 
+    private static final Properties prop = new Properties();
 
+    static {
+        try (FileInputStream fis = new FileInputStream(System.getProperty("user.dir") +
+                "/src/main/java/resources/GlobalData.properties")) {
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load GlobalData.properties", e);
+        }
+    }
+
+    public static String getGlobalValue(String key) {
+        return prop.getProperty(key);
+    }
 }
