4579c898fd1dec2821fceca28d82e274ec168a0f
vgebrych <v.gebrych@gmail.com>
2025-08-31 20:09:56 +0200

    feat(tests): add tests for invalid login scenarios and delete method verification

--- src/test/java/tests/api/UserAuthenticationTest.java
+++ src/test/java/tests/api/UserAuthenticationTest.java
@@ -4,7 +4,9 @@ import base.BaseTestApi;
 import base.annotations.NeedCleanUp;
 import base.annotations.NeedUser;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import testUtils.ApiTestUtils;
 
@@ -14,10 +16,23 @@ import static io.restassured.RestAssured.given;
 @NeedCleanUp
 public class UserAuthenticationTest extends BaseTestApi {
 
+    @DataProvider(name = "invalidLoginDetails")
+    public Object[][] invalidLoginDetails() {
+        return new Object[][]{
+                {getPreconditionUser().getEmail(), "wrongPassword", "404", "User not found!"},
+                {"somewrongemail@gmail.com", getPreconditionUser().getPassword(), "404", "User not found!"},
+                {"somewrongemail@gmail.com", "wrongPassword", "404", "User not found!"},
+                {"", "", "400", "Bad request, email or password parameter is missing in POST request."},
+                {null, null, "400", "Bad request, email or password parameter is missing in POST request."},
+                {getPreconditionUser().getEmail(), "", "400", "Bad request, email or password parameter is missing in POST request."},
+                {"", getPreconditionUser().getPassword(), "400", "Bad request, email or password parameter is missing in POST request."}
+        };
+    }
+
     private final String verifyUserApiPath = "verifyLogin";
 
-    @Test (testName = "API 7: POST To Verify Login with valid details")
-    public void verifyUserLogin(){
+    @Test(testName = "API 7: POST To Verify Login with valid details")
+    public void verifyUserLogin() {
 
         Response verifyUserResponse = given()
                 .contentType("application/x-www-form-urlencoded")
@@ -28,19 +43,25 @@ public class UserAuthenticationTest extends BaseTestApi {
 
         Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "responseCode"), "200"
                 , "Response code should be 200");
-        Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "message"),"User exists!");
+        Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "message"), "User exists!");
     }
 
-    @Test (testName = "API 8: POST To Verify Login without email parameter")
-    public void verifyLoginWithoutEmail(){
-        Response verifyUserResponse = given()
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("password", getPreconditionUser().getPassword())
-                .when().post(verifyUserApiPath)
-                .then().extract().response();
-        Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "responseCode"), "400"
-                , "Response code should be 200");
-        Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "message")
-                ,"Bad request, email or password parameter is missing in POST request.");
+    @Test(testName = "API 9: DELETE To Verify Login")
+    public void deleteToVerifyLogin() {
+        ApiTestUtils.verifyMethodNotSupported(verifyUserApiPath, "DELETE", "405",
+                "This request method is not supported.");
+    }
+
+    @Test(dataProvider = "invalidLoginDetails", testName = "API 8: POST To Verify Login without email parameter AND API 10: POST To Verify Login with invalid details")
+    public void verifyLoginWithInvalidDetails(String email, String password, String code, String message) {
+        RequestSpecification request = given().contentType("application/x-www-form-urlencoded");
+
+        if (email != null && !email.isEmpty()) request.formParam("email", email);
+        if (password != null && !password.isEmpty()) request.formParam("password", password);
+
+        Response verifyUserResponse = request.post(verifyUserApiPath).then().extract().response();
+
+        Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "responseCode"), code);
+        Assert.assertEquals(ApiTestUtils.getValueFromJson(verifyUserResponse, "message"), message);
     }
 }
